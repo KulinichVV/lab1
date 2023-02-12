372bc21a2a5c9ac58d0167c4dbc654bda5843f96
KulinichVV <kulinich_vv@ffeks.dnu.edu.ua>
2023-02-12 22:11:15 +0200

    fixed code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <windows.h>
 #include <string>
 
-using namespase std;
+using namespace std;
 int main()
 {
-	
-        SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+	string a = "Как дела?";
 	cout << "Привет, мир!" << endl;
+	cout << a << endl;
 
 	return 0;
 }
