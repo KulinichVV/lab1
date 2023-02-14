27f36b5a6d8c2a88b1b4796dc16a27fb116a9422
KulinichVV <kulinich_vv@ffeks.dnu.edu.ua>
2023-02-14 11:51:22 +0200

    Fixed 2.2

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,13 @@
 
 using namespace std;
 int main()
-{
-	SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
 	string a = "Как дела?";
 	cout << "Привет, мир!" << endl;
 	cout << a << endl;
 
 	return 0;
+{
+	SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+	system("color 0A");
 }
